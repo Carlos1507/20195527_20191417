02d1ed0f21606a86f95e8d71986471df0e479600
Loammi Jezreel Ugaz Manayay <a20191417@pucp.edu.pe>
2022-06-06 20:04:53 -0500

    Agrego método convertir_Nombres

--- src/main/java/com/example/_20195527_20191417/daos/problem1Dao.java
+++ src/main/java/com/example/_20195527_20191417/daos/problem1Dao.java
@@ -4,11 +4,12 @@ import com.example._20195527_20191417.beans.ActoresBean;
 
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.Locale;
 
 public class problem1Dao {
     private String user= "root";
     private String pass= "root";
-    private String url = "jdbc:mysql://localhost:3306/hr";
+    private String url = "jdbc:mysql://localhost:3306/sakila";
 
     public ArrayList<ActoresBean> listar_ActoresVersa(){
         ArrayList<ActoresBean> listaActores= new ArrayList<ActoresBean>();
@@ -24,6 +25,7 @@ public class problem1Dao {
                     actor.setNombre(rs.getString(2));
                     actor.setCantCategorias(rs.getInt(3));
                     actor.setCantPeliculas(rs.getInt(4));
+                    actor.setNombre(convertir_Nombres(actor.getNombre()));
                     listaActores.add(actor);
                 }
             }
@@ -54,5 +56,31 @@ public class problem1Dao {
                 "having `Cantidad Peliculas`>20 and `Cantidad Categorias`>12 " +
                 "order by `ID`";
         return sql;
+    }
+    public String convertir_Nombres(String nombre){
+        String[] a;
+        a= nombre.split(" ");
+        String x= a[0], y= a[1];
+        String[] x_new= x.split("");
+        String[] y_new= y.split("");
+        x="";
+        y=" ";
+        int i=0;
+        while(i<x_new.length){
+            if(i!=0){
+                x_new[i]=x_new[i].toLowerCase(Locale.ROOT);
+            }
+            x+=x_new[i];
+            i++;
+        }
+        i=0;
+        while(i<y_new.length){
+            if(i!=0){
+                y_new[i]=y_new[i].toLowerCase(Locale.ROOT);
+            }
+            y+=y_new[i];
+            i++;
+        }
+        return x+y;
     }
 }
